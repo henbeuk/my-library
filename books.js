202712b5307fdeec3cd66a4a3cc79e0889fd1ee7
henbeuk <99474644+henbeuk@users.noreply.github.com>
2025-12-28 20:00:30 +0200

    Create books.js

---
+++ books.js
@@ -0,0 +1,11 @@
+const books = [
+  {
+    title: "The Dragonbone Chair",
+    author: "Tad Williams",
+    series: "Memory, Sorrow and Thorn",
+    bookNumber: 1,
+    isbn: "9780756404079",
+    read: false,
+    notes: ""
+  }
+];
